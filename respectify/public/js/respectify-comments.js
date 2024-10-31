db388caf0974ae633f9f7b38e4752a491097adb8
David Millington <vintagedave@gmail.com>
2024-10-31 22:53:35 +0200

    Longer test for JS

--- respectify/public/js/respectify-comments.js
+++ respectify/public/js/respectify-comments.js
@@ -5,12 +5,21 @@ jQuery(document).ready(function($) {
         var form = $(this);
         var formData = form.serialize();
 
-        $.post(form.attr('action'), formData, function(response) {
-            if (response.success) {
-                form.unbind('submit').submit();
-            } else {
-                // Display the error message
-                $('#commentform .comment-form-comment').append('<p class="respectify-error-message">' + response.data + '</p>');
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: formData,
+            success: function(response) {
+                if (response.success) {
+                    form.unbind('submit').submit();
+                } else {
+                    // Display the error message
+                    $('.respectify-error-message').remove(); // Remove any existing error messages
+                    $('#commentform .comment-form-comment').append('<p class="respectify-error-message">' + response.data + '</p>');
+                }
+            },
+            error: function(xhr, status, error) {
+                console.log('AJAX error:', status, error);
             }
         });
     });
